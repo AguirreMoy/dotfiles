1e84ced86d3366a52f654c509853368f3ecef20e
AguirreMoy <aguirre.moy@gmail.com>
2024-01-22 22:27:13 -0800

    :sparkles: Starship

--- mac_install.sh
+++ mac_install.sh
@@ -1,2 +1,6 @@
 brew install font-victor-mono-nerd-font
-brew install nvim
+brew install nvim:q
+
+brew install starship
+echo "starship init fish | source"" >> ~/.config/fish/config.fish
+starship preset pastel-powerline -o ~/.config/starship.toml
